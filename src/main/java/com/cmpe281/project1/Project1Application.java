847f6defc7cc37d1e8fd937049d4afac6f9c1da8
serdardemirci55 <89255090+serdardemirci55@users.noreply.github.com>
2021-10-17 09:44:29 +0300

    CORS config
    allowedOrigins for Methods ("GET", "POST", "DELETE")

--- src/main/java/com/cmpe281/project1/Project1Application.java
+++ src/main/java/com/cmpe281/project1/Project1Application.java
@@ -4,11 +4,14 @@ import com.cmpe281.project1.authorization.JwtTokenFilter;
 import com.cmpe281.project1.authorization.JwtTokenProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Project1Application {
@@ -33,5 +36,15 @@ public class Project1Application {
                     .anyRequest().hasAuthority("myAuthority");
             http.addFilterBefore(new JwtTokenFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
         }
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "DELETE");
+            }
+        };
     }
 }
